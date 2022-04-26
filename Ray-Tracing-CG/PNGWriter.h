7ece37ffc7e4705577aaee9c91d8d1b8c08d3b39
DorianMalguid <dorian.malguid@outlook.fr>
2022-04-26 21:11:24 +0200

    Commentaires et retrait de l’ancienne méthode d’écriture de fichier ppm

--- Ray-Tracing-CG/PNGWriter.h
+++ Ray-Tracing-CG/PNGWriter.h
@@ -4,14 +4,18 @@
 #include "stb_image_write.h"
 
 
+// Classe en charge d’écrire un fichier png à partir du tableau de flottants contenant les couleurs des pixels, en utilisant
+// une seule méthode statique publique.
 class PNGWriter {
 public:
+	// Cache l’appel à stb
 	static void write_file(const char* name, int width, int height, int channels, float* data, int data_size) {
 		unsigned char* integer_data = convert_data(data, data_size);
 		stbi_write_png(name, width, height, channels, integer_data, width * channels);
 	}
 
 private:
+	// Convertit le tableau de flottants en tableau d’entiers 8 bits pour stb
 	static unsigned char* convert_data(float* data, int data_size) {
         unsigned char* out = new unsigned char[data_size];
 		for (int i = 0; i < data_size; i++)
--- Ray-Tracing-CG/main.cu
+++ Ray-Tracing-CG/main.cu
@@ -31,27 +31,5 @@ int main()
     
     PNGWriter::write_file("image.png", IMAGE_WIDTH, IMAGE_HEIGHT, 3, colour_buf, buf_size);
 
-    //write_image_file("image.ppm", colour_buf, buf_size);
-
     return 0;
-}
-
-
-
-// Write the colour buffer into a ppm file
-void write_image_file(string file_name, float* colour_buf, int buf_size) {
-    ofstream out(file_name);
-
-    out << "P3" << endl << IMAGE_WIDTH << " " << IMAGE_HEIGHT << endl << "255" << endl;
-
-    int i = 0;
-
-    while (i < buf_size) {
-        int ir = int(255.999f * colour_buf[i++]);
-        int ig = int(255.999f * colour_buf[i++]);
-        int ib = int(255.999f * colour_buf[i++]);
-
-        out << ir << " " << ig << " " << ib << endl;
-    }
-    out.close();
 }
