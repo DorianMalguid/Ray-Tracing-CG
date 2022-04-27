a93716300c913d9c99f9c54707d29081965b2be2
DorianMalguid <dorian.malguid@outlook.fr>
2022-04-27 20:36:09 +0200

    Premier rendu sur cg

--- Ray-Tracing-CG/main.cu
+++ Ray-Tracing-CG/main.cu
@@ -12,24 +12,37 @@ using namespace std;
 const int IMAGE_WIDTH = 256;
 const int IMAGE_HEIGHT = 256;
 
-void write_image_file(string file_name, float* colour_buf, int buf_size);
+const int THREAD_BLOCK_SIZE = 8;
+
+
+__global__ void render(float* buffer, int width, int height) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (i >= width || j >= height)
+        return;
+
+    int n = 3 * (i + j * width);
+    buffer[n] = float(i) / (width - 1);
+    buffer[n + 1] = float(height - j) / (height - 1);
+    buffer[n + 2] = 0.25f;
+}
 
 int main()
 {
     int buf_size = 3 * IMAGE_WIDTH * IMAGE_HEIGHT;
-    float* colour_buf = new float[buf_size];
+    float* colour_buf;
+    cudaMallocManaged((void**) &colour_buf, buf_size*sizeof(float));
 
-    int k = 0;
-    for (int j = IMAGE_HEIGHT - 1; j >= 0; j--) {
-        for (int i = 0; i < IMAGE_WIDTH; i++)
-        {
-            colour_buf[k++] = float(i) / (IMAGE_WIDTH - 1);
-            colour_buf[k++] = float(j) / (IMAGE_HEIGHT - 1);
-            colour_buf[k++] = 0.25f;
-        }
-    }
-    
+    dim3 blocks(1 + IMAGE_WIDTH / THREAD_BLOCK_SIZE, 1 + IMAGE_HEIGHT / THREAD_BLOCK_SIZE);
+    dim3 threads(THREAD_BLOCK_SIZE, THREAD_BLOCK_SIZE);
+
+    render<<<blocks, threads>>>(colour_buf, IMAGE_WIDTH, IMAGE_HEIGHT);
+    cudaDeviceSynchronize();
+
     PNGWriter::write_file("image.png", IMAGE_WIDTH, IMAGE_HEIGHT, 3, colour_buf, buf_size);
+
+    cudaFree(colour_buf);
+    
 
     return 0;
 }
